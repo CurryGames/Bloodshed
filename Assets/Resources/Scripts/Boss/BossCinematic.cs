9750963368ef3268e2df3c394e2071693fecdc99
dani <dani>
2015-05-14 18:45:14 +0200

    cinematic logic completed!

--- Assets/Resources/Scripts/Boss/BossCinematic.cs
+++ Assets/Resources/Scripts/Boss/BossCinematic.cs
@@ -3,13 +3,19 @@ using System.Collections;
 
 public class BossCinematic : MonoBehaviour {
 
-    public enum CineBehaviour { IDLE, SHOOT, GETGUN}
+    public enum CineBehaviour { IDLE, SHOOT, GETGUN, GAMEPLAY}
     public CineBehaviour behav;
     private float counter = 0;
     private BossStats bStats;
     private BossMove bMove;
+	private BossCinematic bCine;
     private Vector3 gunPos;
 	public GameObject headshotFX;
+	public GameObject wifeDead;
+	public GameObject wife;
+	public GameObject bloodSplash;
+	public GameObject machinegun;
+	private bool shot = false;
 	// Use this for initialization
 	void Start () {
         behav = CineBehaviour.IDLE;
@@ -18,7 +24,8 @@ public class BossCinematic : MonoBehaviour {
 
         gunPos = new Vector3( -2.1f, transform.position.y, 12);
         bMove.enabled = false;
-        bStats.enabled = false;;
+        bStats.enabled = false;
+		bCine = GetComponent<BossCinematic> ();
 	}
 	
 	// Update is called once per frame
@@ -28,9 +35,11 @@ public class BossCinematic : MonoBehaviour {
             {
                 case CineBehaviour.IDLE:
                 {
+					// Set iddle
                     counter += Time.deltaTime;
                     if (counter >= 3)
                     {
+						// Set Shooting
                         behav = CineBehaviour.SHOOT;
                         counter = 0;
                     }
@@ -41,12 +50,22 @@ public class BossCinematic : MonoBehaviour {
                 {
                     counter += Time.deltaTime;
 					
-                    if (counter >= 2)
+                    if (counter >= 2 && counter < 3)
                     {
-						BossShooting();
-                        behav = CineBehaviour.GETGUN;
-                        counter = 0;
+						if (!shot)
+						{
+							BossShooting();
+							shot = true;
+						}
+						Invoke ("WifeDead", 0.12f);
+                       
                     }
+					else if (counter >= 3)
+					{
+						// Set iddle
+						behav = CineBehaviour.GETGUN;
+						counter = 0;
+					}
                 } break;
 
             case CineBehaviour.GETGUN:
@@ -56,18 +75,52 @@ public class BossCinematic : MonoBehaviour {
                     //transform.Rotate(new Vector3(transform.rotation.x, 180, transform.rotation.z) * Time.deltaTime);
                     transform.rotation = Quaternion.Euler( Vector3.zero);
                     transform.position = Vector3.MoveTowards(transform.position, gunPos, 3f * Time.deltaTime);
-                    if (counter >= 3)
+					if (counter >= 1 && counter < 1.5f)
+					{
+						//set shooting (grab weapon)
+					}
+                    if (counter >= 1.5f)
                     {
+						// Set machinegun
+						DestroyMachinegun();
 						transform.rotation = Quaternion.Euler( new Vector3(0, 180, 0));
 						transform.position = Vector3.MoveTowards(transform.position, new Vector3 (0, 0, 7), 3f * Time.deltaTime);
+						counter = 0;
+						behav = CineBehaviour.GAMEPLAY;
                     }
                 } break;
+
+			case CineBehaviour.GAMEPLAY:
+			{
+				counter +=Time.deltaTime;
+				if (counter >= 1) 
+				{
+					bMove.enabled = true;
+					bStats.enabled = true;
+					bCine.enabled = false;
+				}
+			} break;
+
             default: break;
             }
 	}
 
    	public void BossShooting()
 	{
-		Instantiate ( headshotFX, (transform.position + new Vector3(-0.5f , 1.3f, -2f)), Quaternion.Euler (new Vector3 (-90, 45, 0)));
+		Instantiate ( headshotFX, (transform.position + new Vector3(-0.5f , 1.3f, -2.5f)), Quaternion.Euler (new Vector3 (-90, 65, 0)));
+
+		Destroy (headshotFX, 1.5f);
+	}
+
+	public void WifeDead()
+	{
+		Instantiate ( wifeDead, (new Vector3(-1.37f , 0.5f, 7.77f)), Quaternion.Euler (new Vector3 (90, 0, 0)));
+		Destroy (wife, 0);
+		Instantiate ( bloodSplash, (new Vector3(-1.37f , 0.5f, 5.77f)), Quaternion.Euler (new Vector3 (90, 0, 0))) ;
+	}
+
+	public void DestroyMachinegun()
+	{
+		Destroy (machinegun, 0);
 	}
 }
