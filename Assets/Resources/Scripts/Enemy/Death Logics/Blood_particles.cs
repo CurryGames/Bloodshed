8a0a10c84e6d6c1302af73970386454454b66e8c
dMorenov <danielmorenotvjb42013@cev.com>
2015-05-29 04:19:04 +0200

    DeathTest

---
+++ Assets/Resources/Scripts/Enemy/Death Logics/Blood_particles.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Blood_particles : MonoBehaviour {
+
+    private float posX, posZ, rotY;
+    private bool located;
+    public int showChance;
+    private int chance;
+	// Use this for initialization
+	void Start () {
+        posX = Random.Range(-2.4f, 2.4f);
+        posZ = Random.Range(-2f, 2f);
+        rotY = Random.Range(0, 360);
+        located = false;
+        chance = Random.Range(0, 100);
+        if (chance >= showChance) enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (!located)
+        {
+            transform.position = this.transform.position + new Vector3(posX, 0, posZ);
+            transform.rotation = Quaternion.Euler(new Vector3(90, rotY, 0));
+            located = true;
+        }
+
+        
+	}
+}
---
+++ Assets/Resources/Scripts/Enemy/Death Logics/Death_MovePosition.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Death_MovePosition : MonoBehaviour {
+
+    public float force;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+        if (force > 0.1)
+        {
+            transform.Translate(Vector3.up * Time.deltaTime * force);
+            force *= 0.9f;
+        }
+        else force = 0;
+	}
+}
